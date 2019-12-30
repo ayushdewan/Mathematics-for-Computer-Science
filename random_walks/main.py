84a96dc212910c2fb15f49c2dcefb7d78a14385b
Ayush Dewan <ayushdewan02@gmail.com>
2019-12-30 01:29:26 -0800

    performance updates

--- random_walks/main.py
+++ random_walks/main.py
@@ -7,7 +7,7 @@ from matplotlib import animation
 import seaborn as sns
 
 # change grid size, preferably odd values
-nx = ny = 51
+nx = ny = 21
 data = np.zeros((nx, ny))
 
 # change movement directions and probabilities
@@ -31,6 +31,7 @@ def animate_heat_map():
         plt.clf()
 
         # compute next iteration
+        peak = 0
         new_data = np.zeros((nx, ny))
         for i in range(nx):
             for j in range(ny):
@@ -39,13 +40,16 @@ def animate_heat_map():
                     nj = j + dy[k]
                     if ni >= 0 and ni < nx and nj >= 0 and nj < ny:
                         new_data[ni, nj] += bias[k] * data[i, j]
+                        peak = max(peak, new_data[ni, nj])
                     else:
                         new_data[i, j] += bias[k] * data[i, j]
+                        peak = max(peak, new_data[i, j])
+                    
         
         # copy next iteration over
         data = np.copy(new_data)
 
-        ax = sns.heatmap(data, vmin=0, vmax=np.max(data), cmap="YlGnBu")
+        ax = sns.heatmap(data, vmin=0, vmax=peak, cmap="YlGnBu")
 
     anim = animation.FuncAnimation(fig, animate, init_func=init, interval=100)
 
