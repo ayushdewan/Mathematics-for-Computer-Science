e440c15dce14ba6d1de22fefeec266780d0ef9fe
Ayush Dewan <ayushdewan02@gmail.com>
2019-12-29 18:46:24 -0800

    interval update

--- random_walks/main.py
+++ random_walks/main.py
@@ -6,12 +6,18 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 import seaborn as sns
 
+# change grid size, preferably odd values
 nx = ny = 51
 data = np.zeros((nx, ny))
 
+# change movement directions and probabilities
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+bias = [0.125 for i in range(8)]
+assert sum(bias) == 1
+
 def animate_heat_map():
     fig = plt.figure()
-
    
     data[nx // 2, ny // 2] = 1
     ax = sns.heatmap(data, vmin=0, vmax=1, cmap="YlGnBu")
@@ -23,17 +29,12 @@ def animate_heat_map():
     def animate(i):
         global data
         plt.clf()
+
+        # compute next iteration
         new_data = np.zeros((nx, ny))
-        
-        dx = [-1, -1, -1, 0, 0, 1, 1, 1]
-        dy = [-1, 0, 1, -1, 1, -1, 0, 1]
-        bias = [0.125 for i in range(8)]
-
-        assert sum(bias) == 1
-
         for i in range(nx):
             for j in range(ny):
-                for k in range(8):
+                for k in range(len(dx)):
                     ni = i + dx[k]
                     nj = j + dy[k]
                     if ni >= 0 and ni < nx and nj >= 0 and nj < ny:
@@ -41,13 +42,12 @@ def animate_heat_map():
                     else:
                         new_data[i, j] += bias[k] * data[i, j]
         
-        for i in range(nx):
-            for j in range(ny):
-                data[i, j] = new_data[i, j]
+        # copy next iteration over
+        data = np.copy(new_data)
 
         ax = sns.heatmap(data, vmin=0, vmax=np.max(data), cmap="YlGnBu")
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init, interval=10)
+    anim = animation.FuncAnimation(fig, animate, init_func=init, interval=100)
 
     plt.show()
 
